ab0ee321a5d005f25004fe831272ddf2238b30e8
ankitshubham97 <ankitshubham97@gmail.com>
2018-06-09 22:26:16 +0530

    Modify reset.py

--- reset.py
+++ reset.py
@@ -1,3 +1,9 @@
 import subprocess
 
 subprocess.call(["rm","-rf","output/"])
+subprocess.call(["rm","-rf","input/"])
+subprocess.call(["rm","-rf","unknown/"])
+subprocess.call(["mkdir", "input"])
+subprocess.call(["mkdir", "unknown"])
+subprocess.call(["touch","input/.keep"])
+subprocess.call(["touch","unknown/.keep"])
